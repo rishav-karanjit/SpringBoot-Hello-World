70dbf4fbb034bf8190a22d923d78c4dcff730bcb
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-12-25 14:05:59 -0800

    Added saving student

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student abc = new Student(
+                    "abc",
+                    "abc@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    22
+            );
+
+            Student def = new Student(
+                    "def",
+                    "def@gmail.com",
+                    LocalDate.of(2001, Month.JANUARY, 5),
+                    21
+            );
+
+            repository.saveAll(
+                    List.of(abc,def)
+            );
+        };
+    }
+}
