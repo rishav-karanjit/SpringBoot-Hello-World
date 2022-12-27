8d5dff550c6dc9b9901c814051072ade2faf9d3e
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-12-26 23:02:54 -0800

    Added put mapping (student update)

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -40,5 +42,24 @@ public class StudentService {
             );
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name,String email){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow( () -> new IllegalStateException(
+                        "student with id" + studentId + " does not exist"
+                ));
+        if(name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+
+        if(email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(),email)){
+            student.setEmail(email);
+        }
     }
 }
